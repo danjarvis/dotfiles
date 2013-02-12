471ba9a0d3294018b8a22dacc76746ee1a1d4ea1
Dan Jarvis <djarvis@kmbs.konicaminolta.us>
2013-02-12 09:47:52 -0500

    update install script for my environment

--- install.sh
+++ install.sh
@@ -7,19 +7,19 @@ if [ -h "${HOME}/.vim" ]; then
 	rm "${HOME}/.vim"
 fi;
 
-if [ -h "${HOME}/.config/i3" ]; then
-	rm "${HOME}/.config/i3"
+if [ -h "${HOME}/.i3/config" ]; then
+	rm "${HOME}/.i3/config"
 fi;
 
-if [ -h "${HOME}/.config/i3status" ]; then
-	rm "${HOME}/.config/i3status"
+if [ -h "${HOME}/.i3status.conf" ]; then
+	rm "${HOME}/.i3status.conf"
 fi;
 
 
 ln -s ${BASE}/vim ${HOME}/.vim
-ln -s ${BASE}/config/i3 ${HOME}/.config/i3
-ln -s ${BASE}/config/i3status ${HOME}/.config/i3status
+ln -s ${BASE}/i3config ${HOME}/.i3/config
+ln -s ${BASE}/i3status.conf ${HOME}/.i3status.conf
 ln -fs ${BASE}/Xdefaults ${HOME}/.Xdefaults
-ln -fs ${BASE}/vim/vimrc ${HOME}/.vimrc
+ln -fs ${BASE}/vimrc ${HOME}/.vimrc
 ln -fs ${BASE}/bashrc ${HOME}/.bashrc
 ln -fs ${BASE}/gitconfig ${HOME}/.gitconfig
